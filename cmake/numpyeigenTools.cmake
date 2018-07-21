13f4adadd5ff3c3152c7a2a16cada295d0f21d09
Francis Williams <francis@fwilliams.info>
2018-07-20 20:25:09 -0400

    /s/NUMPEIGEN_SRC_DIRECTORY/NPE_SRC_DIR/

--- cmake/numpyeigenTools.cmake
+++ cmake/numpyeigenTools.cmake
@@ -1,15 +1,4 @@
-set(NUMPYEIGEN_SRC_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR}/src)
-
-# Empty list of binding file names
-function(bind_function module_name filename)
-  get_filename_component(name ${filename} NAME_WE)
-  get_filename_component(dir ${filename} DIRECTORY)
-  set(generator_target run_function_binding_generator_${module_name}_${name})
-  add_custom_target(${generator_target}
-    DEPENDS ${filename}
-    COMMAND python ${NUMPYEIGEN_SRC_DIRECTORY}/codegen_function.py ${filename} -o ${dir}/${name}.out.cpp)
-  add_dependencies(${module_name} ${generator_target})
-endfunction()
+set(NPE_SRC_DIR ${CMAKE_CURRENT_SOURCE_DIR}/src)
 
 function(npe_add_module target_name)
   message("Current source dir ${CMAKE_CURRENT_SOURCE_DIR}")
@@ -22,8 +11,8 @@ function(npe_add_module target_name)
 
     set(bound_function_source_filename "${CMAKE_CURRENT_BINARY_DIR}/${name}.out.cpp")
     add_custom_command(OUTPUT ${bound_function_source_filename}
-      DEPENDS ${make_module_BINDING_SOURCES} ${NUMPYEIGEN_SRC_DIRECTORY}/codegen_function.py ${NUMPYEIGEN_SRC_DIRECTORY}/codegen_module.py
-      COMMAND python ${NUMPYEIGEN_SRC_DIRECTORY}/codegen_function.py ${binding_source} -o ${bound_function_source_filename}
+      DEPENDS ${make_module_BINDING_SOURCES} ${NPE_SRC_DIR}/codegen_function.py ${NPE_SRC_DIR}/codegen_module.py
+      COMMAND python ${NPE_SRC_DIR}/codegen_function.py ${binding_source} -o ${bound_function_source_filename}
       WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 
     list(APPEND function_sources "${bound_function_source_filename}")
@@ -31,12 +20,12 @@ function(npe_add_module target_name)
 
   set(module_source_filename ${CMAKE_CURRENT_BINARY_DIR}/${target_name}.module.cpp)
   add_custom_command(OUTPUT ${module_source_filename}
-    DEPENDS ${make_module_BINDING_SOURCES} ${NUMPYEIGEN_SRC_DIRECTORY}/codegen_function.py ${NUMPYEIGEN_SRC_DIRECTORY}/codegen_module.py
-    COMMAND python ${NUMPYEIGEN_SRC_DIRECTORY}/codegen_module.py -o ${module_source_filename} -m ${target_name} -f ${make_module_BINDING_SOURCES}
+    DEPENDS ${make_module_BINDING_SOURCES} ${NPE_SRC_DIR}/codegen_function.py ${NPE_SRC_DIR}/codegen_module.py
+    COMMAND python ${NPE_SRC_DIR}/codegen_module.py -o ${module_source_filename} -m ${target_name} -f ${make_module_BINDING_SOURCES}
     WORKING_DIRECTORY ${CMAKE_CURRENT_SOURCE_DIR})
 
-  pybind11_add_module(${target_name} SHARED ${module_source_filename} ${NUMPYEIGEN_SRC_DIRECTORY}/numpyeigen_typedefs.cpp ${function_sources})
-  target_include_directories(${target_name} PRIVATE ${NUMPYEIGEN_SRC_DIRECTORY})
+  pybind11_add_module(${target_name} SHARED ${module_source_filename} ${NPE_SRC_DIR}/numpyeigen_typedefs.cpp ${function_sources})
+  target_include_directories(${target_name} PRIVATE ${NPE_SRC_DIR})
 
   # python ${make_module_BINDING_SOURCE}
 endfunction()
