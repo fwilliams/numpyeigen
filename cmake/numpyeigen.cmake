d1541f2c998b47103385457c5ab50c91fcf36d6d
Teseo Schneider <teseo@bluewin.ch>
2019-03-04 19:59:33 -0500

    build on windows

--- cmake/numpyeigen.cmake
+++ cmake/numpyeigen.cmake
@@ -202,10 +202,12 @@ function(npe_add_module target_name)
 
   # NumpyEigen uses the C preprocessor for parsing. Here we find a valid command to invoke the C preprocessor
   if (MSVC)
-    set(C_PREPROCESSOR_CMD "\"${CMAKE_CXX_COMPILER}\" /P")
+    set(C_PREPROCESSOR_CMD "\"${CMAKE_CXX_COMPILER}\" /EP")
   else()
     set(C_PREPROCESSOR_CMD "\"${CMAKE_CXX_COMPILER}\" -w -E")
   endif()
+
+  message(STATUS "bla ${C_PREPROCESSOR_CMD}")
 
   # For each binding source file add a "target" which runs the NumpyEigen compiler when the binding code changes
   foreach(binding_source ${npe_add_module_BINDING_SOURCES})
