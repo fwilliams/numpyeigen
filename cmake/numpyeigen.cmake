04a8e6787f224a609472f859e9d8834cb372f435
Teseo Schneider <teseo@bluewin.ch>
2019-03-05 09:10:51 -0500

    build on windows

--- cmake/numpyeigen.cmake
+++ cmake/numpyeigen.cmake
@@ -202,7 +202,7 @@ function(npe_add_module target_name)
 
   # NumpyEigen uses the C preprocessor for parsing. Here we find a valid command to invoke the C preprocessor
   if (MSVC)
-    set(C_PREPROCESSOR_CMD_FLAGS "/EP")
+    set(C_PREPROCESSOR_CMD_FLAGS "/P")
   else()
     set(C_PREPROCESSOR_CMD_FLAGS "-w -E")
   endif()
