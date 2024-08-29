4ffc33b92bdd0b3f467543502bcfd2e1a3bc6832
Francis Williams <fwilliams@users.noreply.github.com>
2024-08-29 16:50:09 -0400

    Update numpyeigenDownloadExternal.cmake

--- cmake/numpyeigenDownloadExternal.cmake
+++ cmake/numpyeigenDownloadExternal.cmake
@@ -36,6 +36,6 @@ endfunction()
 function(numpyeigen_download_pybind11)
     numpyeigen_download_project(pybind11
         GIT_REPOSITORY https://github.com/fwilliams/pybind11.git
-        GIT_TAG        new_numpy_hacks_stable
+        GIT_TAG        new_new_numpy_hacks_stable
     )
 endfunction()
